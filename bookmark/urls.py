e748faf7a0858373eb344ffdaee6990209d5dc8d
Sisi <dqgsh1055@gmail.com>
2019-12-11 20:44:13 +0900

    4.2.6  목록 뷰 만들기 4.2.7  URL 연결하기 4.2.8  bookmark_list.html
    모든 뷰를 클래스 형 뷰로 만듭니다
    
    제네릭 뷰에서는 모델의 오브젝트가 여러개일 경우
    object_list 라는 변수로 전달합니다

---
+++ bookmark/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from bookmark.views import BookmarkListView
+
+urlpatterns = [
+    path('', BookmarkListView.as_view(), name='list'),
+]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from bookmark.models import Bookmark
+
+
+class BookmarkListView(ListView):
+    model = Bookmark
+
+
