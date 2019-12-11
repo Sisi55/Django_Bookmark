79fd1e5a3be3bf35ca5d9dc0482c45431238f9cc
Sisi <dqgsh1055@gmail.com>
2019-12-11 21:39:58 +0900

    4.2.9  북마크 추가 기능 구현
    fields 변수는 어떤 필드들을 입력받을 것인지 설정하는 부분
    success_url은 글쓰기를 완료하고 이동할 페이지
    template_name_suffix 는 사용할 템플릿의 접미사만 변경하는 설정값
    기본으로 설정되어 있는 템플릿 이름은 모델명_xx 형태.
    CreateView, UpdateView는 form이 접미사인데
    이걸 변경해서 bookmark_create 이름의 템플릿 파일을 사용하도록 설정한 것
    
    form 태그의 action 메서드는 자료를 전달할 대상 페이지.
    비워둘 경우 현재 페이지로 전달한다
    form.as_p는 각 필드 폼 태그들을 p 태그로 감싸 출력하는 코드

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from bookmark.views import BookmarkListView
+from bookmark.views import BookmarkListView, BookmarkCreateView
 
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),
+    path('add/', BookmarkCreateView.as_view(), name='add'),
 ]
