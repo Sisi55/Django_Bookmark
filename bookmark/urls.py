18791d8b0ecb64700411bc4f0a9f3011f82c09ae
Sisi <dqgsh1055@gmail.com>
2019-12-11 22:08:32 +0900

    4.2.12  북마크 삭제 기능 구현
    오류 메시지를 참고하여 살펴보니
    템플릿 이름이 bookmark_confirm_delete.html
    삭제할 때는 확인이 필요하기 때문이란다

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from bookmark.views import (
     BookmarkListView, BookmarkCreateView, BookmarkDetailView,
-    BookmarkUpdateView
+    BookmarkUpdateView, BookmarkDeleteView
 )
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add/', BookmarkCreateView.as_view(), name='add'),
     path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
     path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', BookmarkDeleteView.as_view(), name='delete'),
 ]
